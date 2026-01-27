3c948123f0bbda7f6a8efcd4c76458c330877770
alexander garcias <Alexander.garcia@grupocnet.com>
2026-01-27 15:26:58 -0500

    fix endpoints

--- aplicacionmovilserial/Codigo/android/Codigo/lib/config/config_access.dart
+++ aplicacionmovilserial/Codigo/android/Codigo/lib/config/config_access.dart
@@ -1,10 +1,10 @@
 class ConfigEndpointsAccess {
   static String ipAddress = "201.234.153.155";
-  static String pathServerAccess = "https://myscanuma.grupocnet.com/app/api/";
+  static String pathServerAccess = "https://myscanuma.grupocnet.com/api/";
   static List<String> listPathServer = [
     "myscanuma.grupocnet.com (201.234.153.155)"
   ];
   static void modifyServerAccess(String serverAcces) {
-    ConfigEndpointsAccess.pathServerAccess = "https://myscanuma.grupocnet.com/app/api/";
+    ConfigEndpointsAccess.pathServerAccess = "https://myscanuma.grupocnet.com/api/";
   }
 }
