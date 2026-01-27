7914b3c051f66ff2caff3d0aaf2dc4b4edd0d9fd
alexander garcias <Alexander.garcia@grupocnet.com>
2026-01-27 11:48:52 -0500

    nueva ip, verificación endpoint almacenes

--- aplicacionmovilserial/Codigo/android/Codigo/lib/config/config_access.dart
+++ aplicacionmovilserial/Codigo/android/Codigo/lib/config/config_access.dart
@@ -1,17 +1,10 @@
 class ConfigEndpointsAccess {
-  static String ipAddress = "10.240.76.13";
-  static String pathServerAccess = "http://${ConfigEndpointsAccess.ipAddress}/api/";
+  static String ipAddress = "201.234.153.155";
+  static String pathServerAccess = "https://myscanuma.grupocnet.com/app/api/";
   static List<String> listPathServer = [
-    "10.240.76.13",
-    "190.216.201.61",
-    "172.16.101.13",
-    "192.168.76.84",
-    "186.147.143.253",
-    "181.49.162.11"
-
-    //  http://${ConfigEndpointsAccess.ip_address}/api/
+    "myscanuma.grupocnet.com (201.234.153.155)"
   ];
   static void modifyServerAccess(String serverAcces) {
-    ConfigEndpointsAccess.pathServerAccess = "http://${ConfigEndpointsAccess.ipAddress}/api/";
+    ConfigEndpointsAccess.pathServerAccess = "https://myscanuma.grupocnet.com/app/api/";
   }
 }
